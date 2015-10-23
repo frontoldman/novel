b5f564743cc2c8b091431664356bfc91d0021854
luck_zr <529130510@qq.com>
2015-10-23 20:05:20 +0800

    init

---
+++ config.js
@@ -0,0 +1,17 @@
+/**
+ * Created by zhangran on 15/10/23.
+ */
+
+module.exports = {
+    //db:{
+    //    host:'http://139.196.33.138:3306/',
+    //    user:'root',
+    //    pass:'Ddz1234567'
+    //}
+    db:{
+        host:'127.0.0.1',
+        port:3306,
+        user:'root',
+        pass:'zhimatech'
+    }
+};
