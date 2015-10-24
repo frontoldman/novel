a95e4c3a8944370db4556ab6827bf2fba354a58a
luck_zr <529130510@qq.com>
2015-10-24 23:04:53 +0800

    add model

--- index.js
+++ index.js
@@ -2,54 +2,19 @@
  * Created by zhangran on 15/10/23.
  */
 
-var Stream = require('stream')
 var koa = require('koa');
 var route = require('koa-route');
-var request = require('request');
-var iconv = require('iconv-lite');
-var cheerio = require('cheerio');
 var lib = require('./lib/index');
 
 global.config = require('./config');
 
-lib();
+var libs = lib();
 
 var app = koa();
 
 app.use(route.get('/list', function *(next) {
-    var text = yield getText();
-    var list = yield analysisText(text);
-    this.body = list;
+    this.body = 'success';
 }));
-
-function getText() {
-    return new Promise(function(resolve,reject){
-        var stream = request({
-            url:'http://tianyibook.com/tianyibook/42/42073/index.html'
-        });
-        var bufs = [];
-        stream.on('data', function(d){ bufs.push(d); });
-        stream.on('end', function() {
-            var buf = Buffer.concat(bufs);
-            var str = iconv.decode(buf,'gbk');
-            resolve(str);
-        })
-    });
-}
-
-function analysisText(text){
-    var $ = cheerio.load(text);
-    var list = $('table tr td.ccss a');
-    var titles = list.map((index,item) => ({
-        title:$(item).text(),
-        link:$(item).attr('href')
-    }));
-
-
-    return function(fn){
-        fn(null,'hi');
-    }
-}
 
 app.listen(5000);
 console.log('listening on port 5000');
